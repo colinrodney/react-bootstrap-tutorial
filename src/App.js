1f4b61d469d19e1e154ee286f7a663e9e9e6a709
colinrodney <c.renauld.rodney@gmail.com>
2022-08-04 08:15:59 -0500

    playing around w/ stuff

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react"
-import logo from './logo.svg';
 import './App.css';
 // import 
 import "bootstrap/dist/css/bootstrap.min.css"
@@ -8,7 +7,7 @@ import "bootstrap/dist/css/bootstrap.min.css"
 
 
 
-import {Button, Alert} from 'react-bootstrap'
+import {Button, Form} from 'react-bootstrap'
 
 
 
@@ -16,11 +15,28 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <Alert variant="secondary">This is a button!</Alert>
-          <Button>
-            Test Button
-          </Button>
+
       </header>
+      {/* FORM */}
+      <Form style={{backgroundColor: "#eee"}}>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+
+      
     </div>
   );
 }
