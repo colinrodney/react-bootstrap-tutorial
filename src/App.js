2a883485ee80151c3f425155e6912d82fa9e1adc
Colin Rodney <colin.rodney@ibm.com>
2022-08-04 21:54:59 -0500

    testing things yet again...

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react"
 import logo from './logo.svg';
 import './App.css';
+import "./test.css"
 // import 
 import "bootstrap/dist/css/bootstrap.min.css"
 // import single Button
@@ -8,7 +9,7 @@ import "bootstrap/dist/css/bootstrap.min.css"
 
 
 
-import {Button, Alert} from 'react-bootstrap'
+import {Button, Alert, Card} from 'react-bootstrap'
 
 
 
@@ -16,11 +17,21 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src="https://media.istockphoto.com/photos/african-american-hippy-with-afro-gesturing-peace-picture-id173595072?s=612x612"/>
       <Alert variant="secondary">This is a button!</Alert>
           <Button>
             Test Button
           </Button>
       </header>
+      {/* <Card>
+        <Card.Img variant="top" src={"https://source.unsplash.com/UC0HZdUitWY"}/>
+        <Card.Body>
+          <Card.Title>Something...</Card.Title>
+          <Card.Text>
+            The quick brown fox jumped over the moon.
+          </Card.Text>
+        </Card.Body>
+      </Card> */}
     </div>
   );
 }
